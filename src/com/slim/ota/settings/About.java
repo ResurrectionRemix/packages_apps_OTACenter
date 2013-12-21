57f6736ddee6a44e728796cb413d76f13c3d6ee7
dsmitty166 <dsmitty166@gmail.com>
2013-12-21 14:02:44 -0600

    Initial Commit

---
+++ src/com/slim/ota/settings/About.java
@@ -0,0 +1,76 @@
+/*=========================================================================
+ *
+ *  PROJECT:  SlimRoms
+ *            Team Slimroms (http://www.slimroms.net)
+ *
+ *  COPYRIGHT Copyright (C) 2013 Slimroms http://www.slimroms.net
+ *            All rights reserved
+ *
+ *  LICENSE   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ *
+ *  AUTHORS:     fronti90, mnazim, tchaari, kufikugel
+ *  DESCRIPTION: SlimOTA keeps our rom up to date
+ *
+ *=========================================================================
+ */
+
+package com.slim.ota.settings;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+import com.slim.center.MahdiCenter;
+import com.slim.ota.R;
+
+public class About extends Activity {
+    @SuppressWarnings("unused")
+    private static final String TAG = "About";
+
+    private static final int ID_HOME_URL = R.id.about_home;
+    private static final int ID_CONTACT = R.id.about_contact;
+    private static final int ID_LICENSE = R.id.about_license_url;
+
+    private TextView mHomeURL;
+    private TextView mContact;
+    private TextView mLicense;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+
+        mHomeURL = (TextView) findViewById(ID_HOME_URL);
+        mContact = (TextView) findViewById(ID_CONTACT);
+        mLicense = (TextView) findViewById(ID_LICENSE);
+
+        mHomeURL.setMovementMethod(LinkMovementMethod.getInstance());
+        mContact.setMovementMethod(LinkMovementMethod.getInstance());
+        mLicense.setMovementMethod(LinkMovementMethod.getInstance());
+
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // app icon in action bar clicked; go home
+                Intent intent = new Intent(this, MahdiCenter.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
---
+++ src/com/slim/ota/settings/Settings.java
@@ -0,0 +1,113 @@
+/*=========================================================================
+ *
+ *  PROJECT:  SlimRoms
+ *            Team Slimroms (http://www.slimroms.net)
+ *
+ *  COPYRIGHT Copyright (C) 2013 Slimroms http://www.slimroms.net
+ *            All rights reserved
+ *
+ *  LICENSE   http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+ *
+ *  AUTHORS:     fronti90, mnazim, tchaari, kufikugel
+ *  DESCRIPTION: SlimOTA keeps our rom up to date
+ *
+ *=========================================================================
+ */
+
+package com.slim.ota.settings;
+
+import android.app.AlarmManager;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceScreen;
+import android.preference.PreferenceActivity;
+
+import com.slim.ota.updater.UpdateListener;
+import com.slim.ota.R;
+
+import com.commonsware.cwac.wakeful.WakefulIntentService;
+
+public class Settings extends PreferenceActivity implements
+        Preference.OnPreferenceChangeListener {
+    @SuppressWarnings("unused")
+    private static final String TAG = "SlimOTASettings";
+
+    private static final String KEY_UPDATE_INTERVAL = "update_interval";
+    private static final String LAST_INTERVAL = "lastInterval";
+
+    private ListPreference mUpdateInterval;
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.slim_ota_settings);
+
+        PreferenceScreen prefs = getPreferenceScreen();
+
+        mUpdateInterval = (ListPreference) prefs.findPreference(KEY_UPDATE_INTERVAL);
+        mUpdateInterval.setValueIndex(getUpdateInterval());
+        mUpdateInterval.setSummary(mUpdateInterval.getEntry());
+        mUpdateInterval.setOnPreferenceChangeListener(this);     
+        
+     }
+
+    public boolean onPreferenceChange(Preference preference, Object objValue) {
+        if (preference == mUpdateInterval) {
+            int intervalValue = Integer.valueOf((String) objValue);
+            int index = mUpdateInterval.findIndexOfValue((String) objValue);
+            setUpdateInterval(intervalValue);
+            mUpdateInterval.setSummary(mUpdateInterval.getEntries()[index]);
+            return true;
+        }
+        return false;
+    }
+
+    private void setUpdateInterval(int interval) {
+            boolean enableUpdateCheck = true;
+            switch(interval) {
+                case 0:
+                    UpdateListener.interval = AlarmManager.INTERVAL_DAY;
+                    break;
+                case 1:
+                    UpdateListener.interval = AlarmManager.INTERVAL_HALF_DAY;
+                    break;
+                case 2:
+                    UpdateListener.interval = AlarmManager.INTERVAL_HOUR;
+                    break;
+                case 3:
+                    enableUpdateCheck = false;
+                    break;
+            }
+            if (enableUpdateCheck) {
+                SharedPreferences prefs = getSharedPreferences(LAST_INTERVAL, 0);
+                prefs.edit().putLong(LAST_INTERVAL, UpdateListener.interval).apply();
+                WakefulIntentService.cancelAlarms(this);
+                WakefulIntentService.scheduleAlarms(new UpdateListener(), this, true);
+            } else {
+                SharedPreferences prefs = getSharedPreferences(LAST_INTERVAL, 0);
+                prefs.edit().putLong(LAST_INTERVAL, 1).apply();
+                com.slim.ota.updater.ConnectivityReceiver.disableReceiver(this);
+                WakefulIntentService.cancelAlarms(this);
+            }
+    }
+
+    private int getUpdateInterval() {
+        SharedPreferences prefs = getSharedPreferences(LAST_INTERVAL, 0);
+        long value = prefs.getLong(LAST_INTERVAL,0);
+        int settingsValue;
+        if (value == AlarmManager.INTERVAL_DAY) {
+            settingsValue = 0;
+        } else if (value == AlarmManager.INTERVAL_HALF_DAY || value == 0) {
+            settingsValue = 1;
+        } else if (value == AlarmManager.INTERVAL_HOUR) {
+            settingsValue = 2;
+        } else {
+            settingsValue = 3;
+        }
+        return settingsValue;
+    }
+
+}
